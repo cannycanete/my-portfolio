f1a9ebd700b4dc2df22bed5c4297cefc5c992619
cannycanete <canny.canete@my.smciligan.edu.ph>
2023-10-23 10:46:12 +0800

    clickable nav

--- js/script.js
+++ js/script.js
@@ -23,38 +23,43 @@ slide = (direction) => {
 
 }
 
+// Add event listeners to navigation items
+pageHome.addEventListener('click', () => scrollToPage(0));
+pageAbout.addEventListener('click', () => scrollToPage(1));
+pageContact.addEventListener('click', () => scrollToPage(2));
+
+scrollToPage = (page) => {
+    currentPage = page;
+    translate = -currentPage * translateAmount;
+
+    pages.forEach(
+        page => (page.style.transform = `translateX(${translate}%)`)
+    );
+
+    updateNavActiveState();
+}
+
 updateNavActiveState = () => {
+    // about
     if (currentPage === 1) {
         navItems.classList.remove("white-nav");
+        pageContact.classList.remove("active-page");
         pageHome.classList.remove("active-page");
         pageAbout.classList.add("active-page");
         console.log("this is " + currentPage);
+    // contact
     } else if (currentPage === 2) {
         navItems.classList.remove("white-nav");
+        pageHome.classList.remove("active-page");
         pageAbout.classList.remove("active-page");
         pageContact.classList.add("active-page");
         console.log("this is " + currentPage);
-
+    // home 
     } else {
+        pageAbout.classList.remove("active-page");
         pageContact.classList.remove("active-page");
         pageHome.classList.add("active-page");
         navItems.classList.add("white-nav");
         console.log("this is " + currentPage);
     }
 }
-
-
-
-// const pages = document.querySelectorAll(".page");
-// const translateAmount = 100;
-// let translate = 0;
-
-// slide = (direction) => {
-//     direction === "next" ? translate -= translateAmount : translate += translateAmount;
-
-//     pages.forEach(
-//         pages => (pages.style.transform = `translateX(${translate}%)`)
-//     )
-
-//     console.log(translate);
-// }
